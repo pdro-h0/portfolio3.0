60031f52db0947b3d4667d1a9828f065b8f4c3ba
pdro-h0 <pedro.pho792@gmail.com>
2025-06-21 09:29:11 -0300

    fix: fix readme component width

--- src/components/projetos/Readme.tsx
+++ src/components/projetos/Readme.tsx
@@ -6,8 +6,8 @@ export interface ReadmeProps {
 
 export default function Readme(props: ReadmeProps) {
 	return (
-		<div className="flex flex-col items-stretch p-6 bg-[#101016] border border-zinc-800 rounded-2xl">
-			<div className="prose prose-zinc prose-invert" style={{ maxWidth: "100%" }}>
+		<div className="flex flex-col items-stretch w-full lg:max-w-7xl p-6 bg-[#101016] border border-zinc-800 rounded-2xl">
+			<div className="prose prose-zinc prose-invert break-words" style={{ maxWidth: "100%" }}>
 				<ConteudoMD markdown={props.markdown} />
 			</div>
 		</div>
